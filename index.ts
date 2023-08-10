d088a6939a49188847dc4a9607f300825666d5fc
DoormatIka <trashcanartistlily@gmail.com>
2023-08-10 19:34:22 +0900

    init

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import sharp   from "sharp";
+import fs      from "fs";
+
+const app = express();
+app.use(express.json());
+
+app.post("/convertwebp", express.raw({ type: "*/*" }), async (req, res) => {
+    const p = await sharp(req.body)
+        .png()
+        .toBuffer();
+    res.send(p);
+});
+
+app.listen(3500, () => {
+    console.log(`Connected to localhost:3500.`);
+});
