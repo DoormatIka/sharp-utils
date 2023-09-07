a2a873b520e319d0b9c87972efa8483545cb788e
DoormatIka <trashcanartistlily@gmail.com>
2023-09-07 14:16:52 +0900

    removed annoying console.log message

--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@ import sharp   from "sharp";
 import fs      from "fs";
 
 const app = express();
-app.use(express.json({ limit: "20mb" }));
+app.use(express.raw({ type: "*/*", limit: "20mb" }));
 
-app.post("/convertwebp", express.raw({ type: "*/*", limit: "20mb" }), async (req, res) => {
+app.post("/convertwebp", async (req, res) => {
     const p = await sharp(req.body)
         .png()
         .toBuffer();
